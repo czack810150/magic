2449ad019d41621d1501fe98396b11380e3dcf27
Hiro <suhiro@gmail.com>
2017-10-31 15:46:56 -0400

    vue tip edit

--- resources/views/store/tip/show.blade.php
+++ resources/views/store/tip/show.blade.php
@@ -8,21 +8,22 @@
 <h4>{{$tip->location->name}}</h4>
 <p>{{ $tip->start }} ~ {{$tip->end}}</p>
 
-<form class="form" method="POST" action="/tips/{{ $tip->id }}/update">
+<form class="form" method="POST" action="/tips/{{ $tip->id }}/update" id="root">
 
 		<div class="form-group mx-sm-3">
 			{{ Form::label('tips','Total Tips',['class'=>'mx-sm-3']) }}
-			{{ Form::text('tips',$tip->tips*100) }}
+			<input type="text"  name="tips" class="" v-model="totalTips">
 		</div>
 
 		<div class="form-group mx-sm-3">
-			{{ Form::label('hours','Tippable Hours',['class'=>'mx-sm-3']) }}
-			{{ Form::text('hours',$tip->hours) }}
+			{{ Form::label('hours','Tip Hours',['class'=>'mx-sm-3']) }}
+			{{ Form::text('hours',$tip->hours,['v-model'=>'tipHours']) }}
 		</div>
 
 		<div class="form-group mx-sm-3">
 			{{ Form::label('hourlyTip','Hourly Tips',['class'=>'mx-sm-3']) }}
-			{{ Form::number('hourlyTip',$tip->hourlyTip,['min'=>'0.00','step' => '0.01']) }}
+			
+			<input type="number" min="0.00" step="0.01" name="hourlyTip" v-bind:value="hourlyTip">
 		</div>
 
 
@@ -39,4 +40,23 @@
 <a href="/tips/">Back</a>
 </div>
 </div>
+
+@endsection
+
+@section('pageJS')
+<script>
+	var app = new Vue({
+		el: "#root",
+		data: {
+			totalTips: {{$tip->tips/100}},
+			tipHours: {{$tip->hours}},
+			hourlyTips: {{$tip->hourlyTip}},
+		},
+		computed:{
+			hourlyTip(){
+				return Math.round(this.totalTips / this.tipHours *100)/100;
+			}
+		}
+	})
+</script>
 @endsection
