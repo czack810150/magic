ffdec563dea58914cd9d3e3e7ef803d9a85fc487
Hiro <suhiro@gmail.com>
2018-01-01 10:41:36 -0500

    db sync
    with datetime model change

--- app/Datetime.php
+++ app/Datetime.php
@@ -10,7 +10,8 @@ class Datetime extends Model
     
     public static function periods($year)
     {
-    	$periods = DB::table('payroll_period')->where('year',$year)->get();
+    	//$periods = DB::table('payroll_period')->where('year',$year)->get();
+    	$periods = DB::table('payroll_period')->where('year',2017)->get();
     	$periodOptions = array();
     	foreach($periods as $p){
     		$periodOptions[$p->start] = $p->start.' - '.$p->end;
